a5ca9750d76b3eeffc4fb2aff6c873a44cf68d78
Maykel <shalumov97@gmail.com>
2026-08-31 11:44:41 +0300

    refactor(blog): move sections out of header

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,35 @@
 import HeaderBlog from '@/components/HeaderBlog';
+import Container from '@/components/Container';
+import Education from '@/components/Education/Education';
+import Footer from '@/components/Footer';
+import Resources from '@/components/Resources/Resources';
+import VideoCard from '@/components/VideoCard';
+
+import styles from './page.module.scss';
 
 export default function BlogPage() {
 	return (
 		<main>
 			<HeaderBlog />
+			<Container>
+				<section className={styles.videos}>
+					<VideoCard title='What are points and credits?' duration='0:27' />
+					<VideoCard title='Why a mortgage is so much more than just a rate?' duration='0:34' />
+					<VideoCard title='Why a mortgage is so much more than just a rate?' duration='0:34' />
+					<VideoCard title='What are points and credits?' duration='0:27' />
+					<VideoCard title='Why a mortgage is so much more than just a rate?' duration='0:34' />
+					<VideoCard title='Why a mortgage is so much more than just a rate?' duration='0:34' />
+				</section>
+
+				<div className={styles.resources}>
+					<Resources direction='row' className={styles.blogResources} />
+				</div>
+
+				<div className={styles.education}>
+					<Education limit={10} variant='blog' />
+				</div>
+			</Container>
+			<Footer />
 		</main>
 	);
 }
--- src/components/HeaderBlog/HeaderBlog.tsx
+++ src/components/HeaderBlog/HeaderBlog.tsx
@@ -1,11 +1,7 @@
 import Image from 'next/image';
 import Logo from '../Logo';
-import VideoCard from '@/components/VideoCard';
-import Resources from '@/components/Resources/Resources';
-import Education from '@/components/Education/Education';
 import styles from './HeaderBlog.module.scss';
 import Container from '../Container';
-import Footer from '../Footer';
 
 function HeaderBlog() {
 	return (
@@ -19,37 +15,7 @@ function HeaderBlog() {
 					width={816}
 					height={451}
 				/>
-
-				<section className={styles.videos}>
-					<VideoCard title='What are points and credits?' duration='0:27' />
-					<VideoCard
-						title='Why a mortgage is so much more than just a rate?'
-						duration='0:34'
-					/>
-					<VideoCard
-						title='Why a mortgage is so much more than just a rate?'
-						duration='0:34'
-					/>
-					<VideoCard title='What are points and credits?' duration='0:27' />
-					<VideoCard
-						title='Why a mortgage is so much more than just a rate?'
-						duration='0:34'
-					/>
-					<VideoCard
-						title='Why a mortgage is so much more than just a rate?'
-						duration='0:34'
-					/>
-				</section>
-
-				<div className={styles.resources}>
-					<Resources direction='row' className={styles.blogResources} />
-				</div>
-
-				<div className={styles.education}>
-					<Education limit={10} variant='blog' />
-				</div>
 			</Container>
-			<Footer />
 		</header>
 	);
 }
